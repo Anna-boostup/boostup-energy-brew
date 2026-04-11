4d5601d5959bea374287d331545fe223bf87881b
Zdenekdi <dias.zd@gmail.com>
2026-04-11 21:16:39 +0200

    feat: add mobile UI test suite and integrate it into Playwright configuration and CI pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,27 +39,11 @@ export default defineConfig({
   },
 
   /* Configure projects for major browsers */
-  projects: process.env.CI ? [
+  projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-  ] : [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
     /* Test against mobile viewports. */
     {
       name: 'Mobile Chrome',
