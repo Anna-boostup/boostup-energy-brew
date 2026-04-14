775bd5014308306a50928b141e1812b75e4845c4
GitHub Action <action@github.com>
2026-04-14 09:40:15 +0200

    ci: fix lighthouserc.js syntax to use ES modules (export default) and correct header objects

--- .github/lighthouse/lighthouserc.js
+++ .github/lighthouse/lighthouserc.js
@@ -1,11 +1,11 @@
-module.exports = {
+export default {
   ci: {
     collect: {
       settings: {
-        extraHeaders: JSON.stringify({
+        extraHeaders: {
           'x-vercel-protection-bypass': process.env.VERCEL_AUTOMATION_BYPASS_SECRET,
           'x-vercel-set-bypass-cookie': 'true'
-        })
+        }
       }
     }
   }
