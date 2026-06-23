532faaaec60bf1d63a101f1a78b197e94ff3eb2a
Zdenekdi <dias.zd@gmail.com>
2026-06-23 23:54:06 +0200

    Revert "ui: increase spacing around ingredient tag buttons"
    This reverts commit 9e5b8a71280b9dae60d67602850942ad042c77a0.

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -118,7 +118,7 @@ const HeroSection = () => {
         </div>
 
         {/* Tags row - full width below both columns */}
-        <div className="flex flex-wrap gap-4 sm:gap-6 lg:gap-8 mt-8 justify-center">
+        <div className="flex flex-wrap gap-4 mt-2 justify-center">
           {SITE_CONTENT.hero.tags.map((item) => (
             <button
               key={item.ingredientId}
